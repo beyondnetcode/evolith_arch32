c60177c61a22566e71771c2cbe97e4f4b466acd4
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2026-06-11 22:04:29 -0500

    chore: remove test-sdk.js

--- sdk/cli/test-sdk.js
+++
@@ -1,6 +0,0 @@
-const { Server } = require('@modelcontextprotocol/sdk/server/index.js');
-const { StdioServerTransport } = require('@modelcontextprotocol/sdk/server/stdio.js');
-const { CallToolRequestSchema, ListToolsRequestSchema } = require('@modelcontextprotocol/sdk/types.js');
-console.log('Server:', typeof Server);
-console.log('StdioServerTransport:', typeof StdioServerTransport);
-console.log('ListToolsRequestSchema:', !!ListToolsRequestSchema);
